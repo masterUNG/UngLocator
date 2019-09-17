0edec640fa914cfa31e8c950c04946f1eac33fd1
masterUNG <phrombutr@gmail.com>
2019-09-17 15:05:45 +0700

    ListView Food Success

---
+++ lib/models/food_model.dart
@@ -0,0 +1,16 @@
+class FoodModel {
+  // Field
+  String nameFood,
+      pathURL,
+      nameShop,
+      address,
+      timeOpenCloss,
+      detail,
+      lat,
+      lng,
+      postBy;
+
+  // Constructor
+  FoodModel(this.nameFood, this.pathURL, this.nameShop, this.address,
+      this.timeOpenCloss, this.detail, this.lat, this.lng, this.postBy);
+}
--- lib/screens/show_list_food.dart
+++ lib/screens/show_list_food.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ung_locator/models/food_model.dart';
+import 'package:ung_locator/screens/my_style.dart';
 
 class ShowListFood extends StatefulWidget {
   @override
@@ -6,8 +9,119 @@ class ShowListFood extends StatefulWidget {
 }
 
 class _ShowListFoodState extends State<ShowListFood> {
+  // Explicit
+  List<FoodModel> foodModels = [];
+
+  // Method
+  @override
+  void initState() {
+    super.initState();
+    readFireStore();
+  }
+
+  Future<void> readFireStore() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference = firestore.collection('Food');
+    await collectionReference.snapshots().listen((response) {
+      List<DocumentSnapshot> snapshots = response.documents;
+      for (var snapshot in snapshots) {
+        // String nameFood = snapshot.data['NameFood'];
+        // print('nameFood = $nameFood');
+
+        FoodModel foodModel = FoodModel(
+            snapshot.data['NameFood'],
+            snapshot.data['PathURL'],
+            snapshot.data['NameShop'],
+            snapshot.data['Address'],
+            snapshot.data['TimeOpenClose'],
+            snapshot.data['Detail'],
+            snapshot.data['Lat'],
+            snapshot.data['Lng'],
+            snapshot.data['PostBy']);
+
+        setState(() {
+          foodModels.add(foodModel);
+        });
+      }
+    });
+  }
+
+  Widget foodListView() {
+    return ListView.builder(
+      itemCount: foodModels.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          decoration: index % 2 == 0
+              ? BoxDecoration(color: Colors.orange[100])
+              : BoxDecoration(
+                  color: Colors.orange[300],
+                ),
+          child: Row(
+            children: <Widget>[
+              showFoodImage(index),
+              showText(index),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget showFoodImage(int index) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.4,
+      child: Image.network(
+        foodModels[index].pathURL,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget showText(int index) {
+    return Container(
+      // color: Colors.grey,
+      padding: EdgeInsets.all(10.0),
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.4,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          showNameFood(index),
+          showDetailShot(index),
+        ],
+      ),
+    );
+  }
+
+  Widget showNameFood(int index) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Text(
+          foodModels[index].nameFood,
+          style: TextStyle(
+            fontSize: MyStyle().h1,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget showDetailShot(int index) {
+    String detail = foodModels[index].detail;
+
+    if (detail.length > 80) {
+      detail = detail.substring(0, 80);
+      detail = '$detail ...';
+    }
+
+    return Text(detail);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('This is Show List Food');
+    return foodListView();
   }
 }
